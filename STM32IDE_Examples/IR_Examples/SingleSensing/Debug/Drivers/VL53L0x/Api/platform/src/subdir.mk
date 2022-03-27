7abedcc97f56f2ff621dfc7ae4374973c483c2a3
BrettParker97 <55402976+BrettParker97@users.noreply.github.com>
2022-03-27 10:13:09 -0400

    single sensing update
    -added singling sensing example
    -updated bretts helper to call single sensing
    
    Note: some helper functions are tied to GPIO specific to my board. These will need to be changed in final.

---
+++ STM32IDE_Examples/IR_Examples/SingleSensing/Debug/Drivers/VL53L0x/Api/platform/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/VL53L0x/Api/platform/src/vl53l0x_platform.c \
+../Drivers/VL53L0x/Api/platform/src/vl53l0x_platform_log.c 
+
+OBJS += \
+./Drivers/VL53L0x/Api/platform/src/vl53l0x_platform.o \
+./Drivers/VL53L0x/Api/platform/src/vl53l0x_platform_log.o 
+
+C_DEPS += \
+./Drivers/VL53L0x/Api/platform/src/vl53l0x_platform.d \
+./Drivers/VL53L0x/Api/platform/src/vl53l0x_platform_log.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/VL53L0x/Api/platform/src/%.o: ../Drivers/VL53L0x/Api/platform/src/%.c Drivers/VL53L0x/Api/platform/src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/Brett/Desktop/SingleSensing/Drivers/VL53L0x/Api/core/inc" -I"C:/Users/Brett/Desktop/SingleSensing/Drivers/VL53L0x/Api/platform/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-VL53L0x-2f-Api-2f-platform-2f-src
+
+clean-Drivers-2f-VL53L0x-2f-Api-2f-platform-2f-src:
+	-$(RM) ./Drivers/VL53L0x/Api/platform/src/vl53l0x_platform.d ./Drivers/VL53L0x/Api/platform/src/vl53l0x_platform.o ./Drivers/VL53L0x/Api/platform/src/vl53l0x_platform_log.d ./Drivers/VL53L0x/Api/platform/src/vl53l0x_platform_log.o
+
+.PHONY: clean-Drivers-2f-VL53L0x-2f-Api-2f-platform-2f-src
+
